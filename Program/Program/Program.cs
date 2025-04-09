97c185f6db4a9d2b5d706cadfb6296dddecd6793
Taisiia-Hulko <162361499+Taisiia-Hulko@users.noreply.github.com>
2025-04-09 21:01:21 +0300

    Update Program.cs

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+using System;
+
+enum ColorName
+{
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Purple
+}
 
 class DRomb
 {
     protected int d1;
     protected int d2;
-    protected int c; // колір (наприклад, як int код)
+    protected ColorName c; // тепер тип — ColorName
 
-    public DRomb(int d1, int d2, int color)
+    public DRomb(int d1, int d2, ColorName color)
     {
         this.d1 = d1;
         this.d2 = d2;
@@ -26,7 +35,7 @@ class DRomb
         set { d2 = value; }
     }
 
-    public int Color
+    public ColorName Color
     {
         get { return c; }
     }
@@ -63,11 +72,11 @@ class Program
     {
         DRomb[] rombs = new DRomb[]
         {
-            new DRomb(10, 10, 1),
-            new DRomb(12, 16, 2),
-            new DRomb(8, 8, 3),
-            new DRomb(14, 20, 4),
-            new DRomb(15, 15, 5)
+            new DRomb(10, 10, ColorName.Red),
+            new DRomb(12, 16, ColorName.Green),
+            new DRomb(8, 8, ColorName.Blue),
+            new DRomb(14, 20, ColorName.Yellow),
+            new DRomb(15, 15, ColorName.Purple)
         };
 
         int squareCount = 0;
